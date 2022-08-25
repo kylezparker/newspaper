dcc6fb6adbcb08295e4ebab5034ca60e53fe8d49
Kyle Parker <kyle.z.parker3@gmail.com>
2022-08-25 02:10:15 -0700

    submit

---
+++ heroku_config.sh
@@ -0,0 +1,8 @@
+#!usr/bin/env_bash
+
+
+heroku config:set SECRET_KEY=dG61CYAj--T5kRT0sfj7H2se72WuTUkbVDyUvvOeZ5A
+heroku config:set DEFAULT_FROM_EMAIL=kyle.z.parker3@gmail.com
+heroku config:set EMAIL_HOST=smtp.sendgrid.net
+heroku config:set EMAIL_HOST_USER=apikey
+heroku config:set EMAIL_HOST_PASSWORD=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
